28c4172c97999e2aad6859fe35604dd4b00c82d2
sathish-cyberinnov <112637860+sathish-cyberinnov@users.noreply.github.com>
2024-06-07 21:17:01 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 export function aitChatBotMiddleware (secrets) {
-    return (req, res, next) => {
-        
+    return async (req, res, next) => {
+        if (req.path === '/api/conversation') {
+            const proxy = createProxyMiddleware({
+                target: 'http://ait-query-api.us-east-1.elasticbeanstalk.com/api/conversation',
+                changeOrigin: true,
+                pathRewrite: { '^/api/conversation': '' }
+            });
+
+            try {
+                await new Promise((resolve, reject) => {
+                    proxy(req, res, (err) => {
+                        if (err) return reject(err);
+                        resolve();
+                    });
+                });
+            } catch (err) {
+                next(err);
+            }
+        } else {
+            next();
+        }
     }
 }
