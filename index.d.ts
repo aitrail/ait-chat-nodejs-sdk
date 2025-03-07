69844971371c21cb542c8debee74841a6239818f
sathish-cyberinnov <112637860+sathish-cyberinnov@users.noreply.github.com>
2025-03-07 16:50:45 +0530

    Added private functions type

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,18 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { RequestHandler } from "http-proxy-middleware";
 
 declare interface Secrets {
   clientid: string;
   apiKey: string;
 }
+
+declare function createLambdaProxy(
+  targetUrl: string,
+  path: string,
+  clientid: string
+): RequestHandler;
+
+declare function createProxyMiddleware(options: any): RequestHandler;
 
 declare function aitChatBotMiddleware(
   secrets: Secrets
